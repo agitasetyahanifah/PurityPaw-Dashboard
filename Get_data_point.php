615aee56e1c67642076b202106944ca9f13d4150
MUHAMAD FARIS RAFI <farisrafi2002@student.uns.ac.id>
2023-03-01 16:59:53 +0700

    5

---
+++ Get_data_point.php
@@ -0,0 +1,37 @@
+<table class="table table-striped table-bordered">
+    <thead>
+        <tr>
+           
+             <th width="1%">Peringkat</th>
+			<th>Nama</th>
+            <th>Total Point</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        
+        include "../ajax/db_connection.php";
+    $no= 1;
+        $keyword="";
+        if (isset($_POST['search'])) {
+            $keyword = $_POST['search'];
+        }
+
+        $query = mysqli_query($koneksi,"SELECT username, SUM(point) as jumlah FROM scratch_code  WHERE username LIKE '%".$keyword."%' group by username order by jumlah desc");
+        $hitung_data = mysqli_num_rows($query);
+        if ($hitung_data > 0) {
+            while ($data = mysqli_fetch_array($query)) {
+                ?>
+                <tr>
+                  
+                     <td><?php echo $no++; ?></td>
+                    <td><?php echo $data['username']; ?></td>
+                    <td><?php echo $data['jumlah']; ?></td>
+                </tr>
+            <?php } } else { ?> 
+                <tr>
+                    <td colspan='4' class="text-center">Tidak ada data ditemukan</td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
---
+++ simpan-member.php
@@ -0,0 +1,38 @@
+<?php
+//get data dari form
+$id           = $_POST['id'];
+$name 		  = $_POST['name'];
+$domisili       = $_POST['domisili'];
+$email       = $_POST['email'];
+$phone       = $_POST['phone'];
+$pass		 = $_POST['pass'];
+
+?>
+
+<?php
+$servername = "localhost";
+$username = "n1609672_member";
+$password = "Z5i1ec(6hMa4";
+$dbname = "n1609672_ppcmember";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO members (member_id, name,domisili,email,phone)
+VALUES ('$id', '$name', '$domisili','$email','$phone');";
+$sql .= "INSERT INTO user (user_id, user, password)
+VALUES ('', '$email', 'b32a93e6d7dde643593b4238f05c27b3');";
+
+if ($conn->multi_query($sql) === TRUE) {
+  echo "New records created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+ header("location: index.php");
+?>
